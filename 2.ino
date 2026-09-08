834999f1567c3f51f8de847a94d6e0d4b20566b2
kjh11831 <k38140834@gmail.com>
2026-09-08 20:44:38 +0900

    2단계 개선: delay() 대신 millis() 비차단 제어로 변경
    Replaced delay-based LED blinking with non-blocking millis method to improve responsiveness.

--- 2.ino
+++ 2.ino
@@ -1,17 +1,25 @@
+// 실험 2단계: LED 2개 교대 점멸 (delay -> millis 비차단 방식으로 개선)
+
 #define LED1 22
 #define LED2 23
+
+unsigned long prevMillis = 0;
+const long interval = 1000;   // 1초 간격
+bool state = false;
 
 void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, LOW);
-  delay(1000);
-
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, HIGH);
-  delay(1000);
+  unsigned long now = millis();
+  if (now - prevMillis >= interval) {   // delay() 없이 1초마다 전환
+    prevMillis = now;
+    state = !state;
+    digitalWrite(LED1, state ? HIGH : LOW);
+    digitalWrite(LED2, state ? LOW : HIGH);
+    Serial.println(state);              // 동시에 시리얼 출력 가능
+  }
 }
